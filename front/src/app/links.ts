d7a336d1f33b8b5d3de3f578104d0ebe663290f0
CanseFr <jucansell@gmail.com>
2025-09-08 23:03:09 +0200

    :sparkles: Fix enum

--- front/src/app/links.ts
+++ front/src/app/links.ts
@@ -1,7 +1,9 @@
-export enum LINKS {
-    DASHBOARD = "http://82.165.92.40:3000/",
-    SERVERS = "/servers",
-    ADD_SERVERS = "/add-servers",
-    SHOP = "http://82.165.46.201/",
-    CONFIGURATOR = "http://82.165.44.233/"
-}
+export const LINKS = {
+    DASHBOARD: "http://82.165.92.40:3000/",
+    SERVERS: "/servers",
+    ADD_SERVERS: "/add-servers",
+    SHOP: "http://82.165.46.201/",
+    CONFIGURATOR: "http://82.165.44.233/",
+} as const;
+
+export type LinkKey = keyof typeof LINKS;
