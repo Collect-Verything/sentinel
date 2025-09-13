5c87f691b9e3b23c4f13681330abe58c7c29efab
CanseFr <jucansell@gmail.com>
2025-09-14 00:14:36 +0200

    :sparkles: Setup server service

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ServersModule } from './servers/servers.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ServersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back/src/servers/servers.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateServerDto } from './dto/create-server.dto';
+import { UpdateServerDto } from './dto/update-server.dto';
+import {PrismaService} from "../prisma/prisma.service";
+
+@Injectable()
+export class ServersService {
+    constructor(private prisma: PrismaService) {}
+
+    create(createServerDto: CreateServerDto) {
+    return 'This action adds a new server';
+  }
+
+  findAll() {
+    return this.prisma.server.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} server`;
+  }
+
+  update(id: number, updateServerDto: UpdateServerDto) {
+    return `This action updates a #${id} server`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} server`;
+  }
+}
