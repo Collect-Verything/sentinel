a0c6e24df747a222e0985112b1bdcaf0e976707a
CanseFr <jucansell@gmail.com>
2025-09-20 13:36:13 +0200

    :label: Create server and config create dto type

--- back/src/servers/dto/create-server.dto.ts
+++ back/src/servers/dto/create-server.dto.ts
@@ -1,1 +1,28 @@
-export class CreateServerDto {}
+export class CreateAnsibleConfigDto {
+    id: number
+    name: string
+    editorFullname: string
+    playbookPath: string
+    variablesJson: string
+    servers: CreateServerDto[]
+}
+
+export class CreateServerDto {
+    serverIp: string
+    status: "PENDING" | "CONFIGURING" | "CONFIGURED" | "ERROR"
+    isSsl: boolean | undefined
+    cores: number
+    ramMb: number
+    storageGb: number
+    provider: string
+    ownerClientId: number
+    batchId: number
+    sshUser: string
+    sshPort: number
+    sshAuth: "PASSWORD" | "KEY"
+    sshPassword: string
+    health: "OK" | "WARN" | "UNKNOWN" | "CRIT"
+    lastSeenAt: string
+    lastCheckAt: string
+    ansibleConfig: CreateAnsibleConfigDto
+}
