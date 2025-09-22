da88f53ba233debfba14e657d920f2f1ad55f265
CanseFr <jucansell@gmail.com>
2025-09-22 23:17:41 +0200

    :lipstick: Fix lint deploy element not used

--- front/src/pages/main-layout/index.tsx
+++ front/src/pages/main-layout/index.tsx
@@ -1,7 +1,6 @@
 import {Outlet} from "react-router";
 import {MainAppBar} from "../app-bar";
 import {Tasks} from "../tasks";
-import {TasksProvider} from "../../contexts/TasksContext.tsx";
 
 export const MainLayout = () => {
     return (
